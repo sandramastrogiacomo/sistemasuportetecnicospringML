3d3e8c25392e04f0a80ccb4c539e70a892c1e3b6
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-06-11 09:28:49 -0300

    teste unitario final atendente service

--- src/test/java/com/suporte/sistemasuporte/AtendenteServiceTest.java
+++ src/test/java/com/suporte/sistemasuporte/AtendenteServiceTest.java
@@ -9,7 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,4 +73,56 @@ public class AtendenteServiceTest {
          Optional<AtendenteModel> resultado = atendenteService.findById(1l);
          assertFalse(resultado.isPresent());
      }
+     @Test
+    public void testDelete_Existente() {
+        AtendenteModel atendenteModel = new AtendenteModel();
+        atendenteModel.setId(1l);
+        atendenteModel.setNome("Pedro Figueiredo");
+
+        when(atendenteRepository.findById(1l)).thenReturn(Optional.of(atendenteModel));
+
+        boolean resultado = atendenteService.delete(1l);
+
+        assertTrue(resultado);
+
+        verify(atendenteRepository, times(1)).deleteById(1l);
+    }
+
+    @Test
+    public void testDelete_NaoExistente() {
+
+        when(atendenteRepository.findById(1l)).thenReturn(Optional.empty());
+
+        boolean resultado = atendenteService.delete(1l);
+
+        assertFalse(resultado);
+
+        verify(atendenteRepository, never()).deleteById(anyLong());
+    }
+
+    @Test
+    public void testFindAll() {
+
+        AtendenteModel atendenteModel1 = new AtendenteModel();
+        atendenteModel1.setId(1l);
+        atendenteModel1.setNome("Pedro Figueiredo");
+
+        AtendenteModel atendenteModel2 = new AtendenteModel();
+        atendenteModel2.setId(1l);
+        atendenteModel2.setNome("Ana Machado");
+
+        List<AtendenteModel> lista = List.of(atendenteModel1, atendenteModel2);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<AtendenteModel> pagina = new PageImpl<>(lista, pageable, lista.size());
+
+        when(atendenteRepository.findAll(pageable)).thenReturn(pagina);
+        Page<AtendenteModel> resultado = atendenteService.findAll(pageable);
+
+        assertNotNull(resultado);
+        assertEquals(2, resultado.getTotalElements());
+        assertEquals ("Pedro Figueiredo", resultado.getContent().get(0).getNome());
+        assertEquals ("Ana Machado", resultado.getContent().get(1).getNome());
+
+        verify(atendenteRepository, times(1)).findAll(pageable);
+    }
 }

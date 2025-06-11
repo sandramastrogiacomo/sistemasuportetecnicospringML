a358aad397fe93f95d437c893081b19098a17301
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-06-11 08:41:35 -0300

    "ajustes teste unitario atendenteservice"

--- src/main/java/com/suporte/sistemasuporte/service/AtendenteService.java
+++ src/main/java/com/suporte/sistemasuporte/service/AtendenteService.java
@@ -3,25 +3,20 @@ package com.suporte.sistemasuporte.service;
 import com.suporte.sistemasuporte.dto.AtendenteDTO;
 import com.suporte.sistemasuporte.model.AtendenteModel;
 import com.suporte.sistemasuporte.repository.AtendenteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AtendenteService {
 
-    private final AtendenteRepository repository;
+    private final AtendenteRepository atendenteRepository;
 
     public AtendenteService(AtendenteRepository repository) {
-        this.repository = repository;
+        this.atendenteRepository = repository;
     }
-
-    @Autowired
-    private AtendenteRepository atendenteRepository;
 
     public AtendenteModel create(AtendenteDTO atendenteDTO) {
     AtendenteModel atendente = new AtendenteModel();
